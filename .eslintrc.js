56d5cd2190c301bc6055e118b6cd487bbc7d8ad3
rubenaprikyan <rubenaprikyan@gmail.com>
2023-12-05 22:26:56 +0400

    Initial Commit

---
+++ .eslintrc.js
@@ -0,0 +1,36 @@
+// eslint-disable-next-line
+module.exports = {
+  env: {
+    node: true,
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'prettier',
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+  ],
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint'],
+  ignorePatterns: ['**/dist/**'],
+  rules: {
+    'no-console': 'warn',
+    '@typescript-eslint/no-var-requires': 'off',
+    'no-mixed-spaces-and-tabs': 'off',
+    'prefer-const': 'off',
+    'prefer-rest-params': 'off',
+    '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/no-this-alias': 'off',
+    '@typescript-eslint/no-non-null-assertion': 'off',
+    '@typescript-eslint/ban-ts-comment': 'off',
+    '@typescript-eslint/no-empty-function': 'off',
+    '@typescript-eslint/no-unused-vars': [
+      'error'
+    ],
+    'quotes': [
+      'error',
+      'single'
+    ],
+    'indent': ['error', 2],
+  },
+};
